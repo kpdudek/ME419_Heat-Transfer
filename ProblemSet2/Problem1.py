11069c046ed9e2c0339dbcb9edc76389b03c5e26
kurt <kpdudek@bu.edu>
2019-02-09 11:23:39 -0500

    Problem 1 complete

--- ProblemSet2/Problem1.py
+++ ProblemSet2/Problem1.py
@@ -1,1 +1,70 @@
+#!/usr/bin/env
+
+import numpy as np
+import matplotlib.pyplot as mp
+from matplotlib import rc
+
+#rc('text',usetex=True)
+
+
+###- PLOT 1 -###
+Lf = np.linspace(0,.001,num=15)
+
+q_dot = ((60.0-20.0)/((1.0/50.0)+(Lf/.025))) + ((60.0-30.0)/(.001/.05))
+
+#print(q_dot)
+
+mp.figure()
+mp.plot(Lf,q_dot)
+mp.xlabel('Film Thickness (m)')
+mp.ylabel(r'Heat Flux $(\frac{W}{m^2})$')
+mp.title(r'Transparent Film Thickness vs Heat Flux')
+
+
+
+###- PLOT 2 -###
+q_dot2 = (((60000*Lf+60)-20.0)/((1.0/50.0))) + (((60000*Lf+60)-30.0)/((Lf/.025)+(.001/.05)))
+
+
+mp.figure()
+mp.plot(Lf,q_dot2)
+mp.xlabel('Film Thickness (m)')
+mp.ylabel(r'Heat Flux $(\frac{W}{m^2})$')
+mp.title(r'Opaque Film Thickness vs Heat Flux')
+
+
+# Display figure windows
+mp.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
